03850cbf2b20c2117bf8a4b2bf2939c253ff131a
dev-splin <dev.splin@gmail.com>
2023-01-04 12:52:19 +0900

    ADD : [화면 만들기] 로그인 폼 만들기

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
 import {Menu, Input, Col, Row} from "antd";
+import React, {useState} from "react";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
 
 const {Search} = Input;
 
@@ -19,13 +22,16 @@ const menus = [
   }
 ];
 
+
+
 const AppLayout = ({children}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal" items={menus}/>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {isLoggedIn ? <UserProfile/> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
---
+++ front/components/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserProfile = props => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+UserProfile.propTypes = {
+  
+};
+
+export default UserProfile;
