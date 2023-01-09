11a0dbde8926dc0ef471bfae26a33257cd83f8c9
dev-splin <dev.splin@gmail.com>
2023-01-09 09:12:34 +0900

    CORRECT : Redux - redux 설정 관련 주석 업데이트

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 import Head from "next/head";
 import wrapper from "../store/configureStore";
 
+/**
+ * server에서 페이지를 구성할 때 공통적으로 사용
+ * client 로직은 동작하지 않음 (window 등...)
+ * @param Component
+ * // @param pageProps : getStaticProps, getStaticPaths, getServerSideProps을 사용하여 props를 받을 수 있음
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const NodeBird = ({Component}) => {
   return (
     <>
@@ -19,4 +27,8 @@ NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
+/**
+ * getInitialProps, getServerSideProps등에서 Redux Store 접근을 위하여
+ * next-redux-wrapper를 사용해 App(여기에서는 NodeBird)을 감싸줌
+ */
 export default wrapper.withRedux(NodeBird);
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -3,16 +3,25 @@ import {applyMiddleware, compose, createStore} from "redux";
 import reducer from "../reducers";
 import {composeWithDevTools} from "redux-devtools-extension";
 
+/**
+ * Store 설정.
+ * @returns {Store<S & {}, Action> & Store<S & {}, A> & {dispatch: Dispatch<A>}}
+ */
 const configureStore = () => {
   const middlewares = [];
+
+  // middlewares를 사용하여 redux의 기능을 확장
   const enhancer = process.env.NODE_ENV === "production"
     ? compose(applyMiddleware(...middlewares))
-    : composeWithDevTools(applyMiddleware(...middlewares));
+    : composeWithDevTools(applyMiddleware(...middlewares)); // 개발용일 때는 개발자 도구와 연동(History)
+
+  // store 생성
   const store = createStore(reducer, enhancer);
 
   return store;
 };
 
+// Server Side에서 Redux Store 접근을 위한 wrapper 생성
 const wrapper = createWrapper(configureStore, {
   debug: process.env.NODE_ENV === "development",
 });
