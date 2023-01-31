612bc08430ad52d5591ad36cac8a50a97ff45f05
dev-splin <dev.splin@gmail.com>
2023-01-31 12:48:10 +0900

    Redux 연동 | ADD : 글로벌 스타일과 컴포넌트 폴더 구조

--- front/components/imagesZoom/index.js
+++ front/components/imagesZoom/index.js
@@ -1,88 +1,24 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Slick from 'react-slick'
-import styled from "styled-components";
-
-const Overlay = styled.div`
-  position: fixed;
-  z-index: 5000;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-// styled-components에 사용하는 백틱 문법은 태그드리터럴이라고 하며, 함수 호출 하나의 방법임
-const Header = styled.header` 
-  header: 44px;
-  background: white;
-  position: relative;
-  padding: 0;
-  text-align: center;
-  
-  & h1 {
-    margin: 0;
-    font-size: 17px;
-    color: #333;
-    line-height: 44px;
-  }
-  
-  & button {
-    position: absolute;
-    right: 0;
-    top: 0;
-    padding: 15px;
-    line-height: 14px;
-    cursor: pointer;
-  }
-`;
-
-const SlickWrapper = styled.div`
-  height: calc(100% - 44px);
-  background: #090909;
-`;
-
-const ImageWrapper = styled.div`
-  margin: 0 auto;
-  max-height: 750px;
-  
-  & img {
-    margin: 0 auto;
-    max-height: 750px;
-  }
-`;
-
-const Indicator = styled.div`
-  text-align: center;
-  
-  & > div {
-    width: 75px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 15px;
-    background: #313131;
-    display: inline-block;
-    text-align: center;
-    color: white;
-    font-size: 15px;
-  }
-`;
+import {CloseBtn, Global, Header, ImageWrapper, Indicator, Overlay, SlickWrapper} from "./styles";
 
 const ImagesZoom = ({ images, onClose }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   return (
     <Overlay>
+      <Global/>
       <Header>
         <h1>상세 이미지</h1>
-        <button onClick={onClose}>X</button>
+        <CloseBtn onClick={onClose}>X</CloseBtn>
       </Header>
       <SlickWrapper>
         <div>
           <Slick
             initialSlide={0}
-            afterChange={slide => setCurrentSlide(slide)}
-            infinite
+            beforeChange={slide => setCurrentSlide(slide)}
+            infinite={true}
             arrows={false}
             slidesToShow={1}
             slidesToScroll={1}
@@ -93,6 +29,11 @@ const ImagesZoom = ({ images, onClose }) => {
               </ImageWrapper>
             ))}
           </Slick>
+          <Indicator>
+            <div>
+              {currentSlide + 1} / {images.length}
+            </div>
+          </Indicator>
         </div>
       </SlickWrapper>
     </Overlay>
